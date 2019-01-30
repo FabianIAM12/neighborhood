9b2e8a2283be57061751f49c117cf779993d83f9
FabianIAM12 <fabian.b89@gmail.com>
2019-01-30 14:54:12 +0100

    Added Place Detail component

---
+++ src/components/PlaceDetail.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Stars from "./Container";
+
+
+export class PlaceDetail extends Component {
+
+    render() {
+        const {locationDetails} = this.props;
+
+        return (
+            <div className='info-window'>
+                <h2>Further Informations:</h2>
+                <p className="further_details">{locationDetails.category}</p>
+                <p className="further_details">{locationDetails.address}</p>
+            </div>
+        );
+    }
+}
+
+export default PlaceDetail;
